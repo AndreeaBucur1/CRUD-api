2d9869db0a4a63a9f595bf766807ee7d26068d82
Andreea Bucur <andreeabucur45@yahoo.com>
2022-04-25 17:41:25 +0300

    refactor

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
 import { User } from './user';
 import { UserService } from './user.service';
 
@@ -19,52 +20,69 @@ describe('UserService', () => {
   });
 
 
-  describe('create',() => {
+
+
+  describe('create', () => {
     it('should be created.', () => {
-      const user = new User();
-      user.email = 'email';
+      const user = {email: 'diego@gmail.com' } as User;
+      service['userList'] = [{email: 'david@gmail.com'} as User]
+      const allUsers = [service['userList'][0], user];
+      
+      jest.spyOn(service, 'isEmailTaken').mockReturnValue(false);
 
-      jest
-      .spyOn(service,'isEmailTaken')
-      .mockReturnValue(false);
-
-     expect.assertions(2);
-     expect(service.create(user)).toEqual(user);
-     expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
+      expect.assertions(3);
+      expect(service.create(user)).toEqual(user);
+      expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
+      expect(service.findAll()).toEqual(allUsers)
     });
 
-    it('should return undefined.', () => {
+
+
+    it('should throw error if user already exists.', () => {
       const user = new User();
       user.email = 'email';
 
-      jest
-      .spyOn(service,'isEmailTaken')
-      .mockReturnValue(true);
+      jest.spyOn(service, 'isEmailTaken').mockReturnValue(true);
 
-     expect.assertions(2);
-     expect(service.create(user)).toEqual(undefined);
-     expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
+      expect.assertions(2);
+      expect(
+        () => service.create(user)
+      ).toThrow('Email is already taken')
+      expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
     })
   });
 
-  describe('update',() => {
+  
+  test('test', () => { 
+    const n = null, d = 10, u = undefined, f = false, t = true; 
+ 
+    expect.assertions(6); 
+    expect(n).toBeNull(); 
+    expect(d).toBeDefined(); 
+    expect(u).toBeUndefined(); 
+    expect(t).toBeTruthy(); 
+    expect(f).toBeFalsy(); 
+    expect(f).not.toBeTruthy(); 
+  });
+
+  describe('update', () => {
     it('should be updated.', () => {
       const user = new User();
       user.email = 'email';
       service['userList'] = [new User()]
 
       jest
-      .spyOn(service,'isEmailTaken')
-      .mockReturnValue(false);
+        .spyOn(service, 'isEmailTaken')
+        .mockReturnValue(false);
 
       jest
-      .spyOn(service,'findIndexById')
-      .mockReturnValue(0);
+        .spyOn(service, 'findIndexById')
+        .mockReturnValue(0);
 
-     expect.assertions(3);
-     expect(service.update(0,user)).toEqual(user);
-     expect(service.findIndexById).toHaveBeenCalledWith(0);
-     expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
+      expect.assertions(3);
+      expect(service.update(0, user)).toEqual(user);
+      expect(service.findIndexById).toHaveBeenCalledWith(0);
+      expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
     });
 
     it('should return undefined because id is not found.', () => {
@@ -73,17 +91,17 @@ describe('UserService', () => {
       service['userList'] = []
 
       jest
-      .spyOn(service,'isEmailTaken')
-      .mockReturnValue(false);
+        .spyOn(service, 'isEmailTaken')
+        .mockReturnValue(false);
 
       jest
-      .spyOn(service,'findIndexById')
-      .mockReturnValue(-1);
+        .spyOn(service, 'findIndexById')
+        .mockReturnValue(-1);
 
-     expect.assertions(3);
-     expect(service.update(0,user)).toEqual(undefined);
-     expect(service.findIndexById).toHaveBeenCalledWith(0);
-     expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
+      expect.assertions(3);
+      expect(service.update(0, user)).toEqual(undefined);
+      expect(service.findIndexById).toHaveBeenCalledWith(0);
+      expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
     });
 
     it('should return undefined because email is taken.', () => {
@@ -92,18 +110,18 @@ describe('UserService', () => {
       service['userList'] = [new User()]
 
       jest
-      .spyOn(service,'isEmailTaken')
-      .mockReturnValue(true);
+        .spyOn(service, 'isEmailTaken')
+        .mockReturnValue(true);
 
-     expect.assertions(2);
-     expect(service.update(0,user)).toEqual(undefined);
-     expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
+      expect.assertions(2);
+      expect(service.update(0, user)).toEqual(undefined);
+      expect(service.isEmailTaken).toHaveBeenCalledWith(user.email);
     });
   });
 
-  describe('deleteById',() => {
+  describe('deleteById', () => {
     it('should be deleted.', () => {
-      const userList: User[] = [{id: 1} as User, {id: 2} as User];
+      const userList: User[] = [{ id: 1 } as User, { id: 2 } as User];
 
       service['userList'] = [...userList];
       service.deleteById(1);
@@ -116,28 +134,44 @@ describe('UserService', () => {
     });
   });
 
-  describe('deleteUsers',() => {
+  describe('deleteUsers', () => {
     it('should be deleted.', () => {
-      const userList= [new User(), new User()];
+      const userList = [new User(), new User()];
       userList[0].id = 0;
       userList[1].id = 1;
 
       service['userList'] = [...userList];
-      service.deleteUsers([0,1]);
+      service.deleteUsers([0, 1]);
 
       expect.assertions(1);
       expect(service.findAll())
         .toEqual(
-          []        
+          []
         );
     });
   });
 
-  describe('findSpecialUser', () => {
-    it('Should be userList an empty array.', () => {
+  describe('sum', () => {
+
+    it('Should return the result of the division', () => {
+      expect.assertions(1);
+      expect(service.div(9, 3)).toEqual(3);
+    })
+
+    it('should throw an exception', () => {
+      expect.assertions(1);
+      expect(
+        () => service.div(3, 0)
+      ).toThrow('Cannot divide by 0')
+    })
+
+  })
+
+  describe('findRandomUser', () => {
+    it('Should return undefined if userList is empty.', () => {
       service['userList'] = [];
       expect.assertions(1);
-      expect(service.findSpecialUser()).toEqual(undefined);
+      expect(service.findRandomUser()).toEqual(undefined);
     });
 
     it('Should be userList has users, but the returning user will not be valid.', () => {
@@ -146,13 +180,13 @@ describe('UserService', () => {
       jest
         .spyOn(UserService.prototype as any, 'takeARandomIndex')
         .mockReturnValue(0);
-      
+
       jest
         .spyOn(UserService.prototype as any, 'isAccountValid')
         .mockReturnValue(false);
 
       expect.assertions(3);
-      expect(service.findSpecialUser()).toEqual(undefined);
+      expect(service.findRandomUser()).toEqual(undefined);
       expect(service['takeARandomIndex']).toHaveBeenCalled();
       expect(service['isAccountValid']).toHaveBeenCalledWith(0);
     });
@@ -163,13 +197,13 @@ describe('UserService', () => {
       jest
         .spyOn(UserService.prototype as any, 'takeARandomIndex')
         .mockReturnValue(0);
-      
+
       jest
         .spyOn(UserService.prototype as any, 'isAccountValid')
         .mockReturnValue(true);
 
       expect.assertions(3);
-      expect(service.findSpecialUser()).toEqual({});
+      expect(service.findRandomUser()).toEqual({});
       expect(service['takeARandomIndex']).toHaveBeenCalled();
       expect(service['isAccountValid']).toHaveBeenCalledWith(0);
     });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,23 +7,23 @@ export class UserService {
   private userList: User[] = [];
 
   public create(user: User): User | undefined {
-	if (this.isEmailTaken(user.email)) {
-		return undefined;
-	}
-	this.userList.push(user);
-	return user;
+    if (this.isEmailTaken(user.email)) {
+      throw new Error('Email is already taken')
+    }
+    this.userList.push(user);
+    return user;
   }
 
-  public update(id: number, user: User): User | undefined {
-		if (this.isEmailTaken(user.email)) {
-      return undefined;
-		}
-		const indexUser = this.findIndexById(id);
-		if (indexUser > -1) {
-		  this.userList[indexUser] = user;
+  public update(id: number, user: User): User {
+    if (this.isEmailTaken(user.email)) {
+      throw new Error('Email is already taken')
+    }
+    const indexUser = this.findIndexById(id);
+    if (indexUser > -1) {
+      this.userList[indexUser] = user;
       return user;
-		}
-		return undefined;
+    }
+    return undefined;
   }
 
   public getUserById(id: number): User {
@@ -55,35 +55,42 @@ export class UserService {
     }
   }
 
-  public isEmailTaken(
-    formEmail: string,
-    currentEmail?: string,
-  ): boolean {
+  public isEmailTaken(formEmail: string, currentEmail?: string): boolean {
     if (formEmail === currentEmail) {
       return false;
     }
     return this.userList.some((user) => user.email === formEmail);
   }
 
-  private takeARandomIndex(): number{
+  
+  private isAccountValid(index: number): boolean {
+    return this.userList[index].firstName !== ''
+    && this.userList[index].lastName !== ''
+    && this.userList[index].username !== '';
+  }
+  
+  private takeARandomIndex(): number {
     return Math.floor(Math.random() * this.userList.length);
   }
-
-  private isAccountValid(index: number): boolean{
-    return this.userList[index].firstName !== ''
-      && this.userList[index].lastName !== ''
-      && this.userList[index].username !== '';
-  }
-
-  public findSpecialUser(): User | undefined{
-    if (this.userList.length === 0){
+  
+  public findRandomUser(): User | undefined {
+    if (this.userList.length === 0) {
       return;
     }
 
     const index = this.takeARandomIndex();
-    if (this.isAccountValid(index)){
+    if (this.isAccountValid(index)) {
       return this.userList[index];
     }
     return;
-  }  
+  }
+
+  public div(a: number, b: number) {
+    if (b === 0) {
+      throw new Error('Cannot divide by 0');
+    }
+    return a / b;
+  }
+
+
 }
